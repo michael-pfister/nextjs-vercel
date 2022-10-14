c1e586c4f3f73d8272b0ffc0f637ebc47b488d86
stracciatella27 <schoko.bombe44@gmail.com>
2022-10-14 12:17:44 +0200

    add test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,9 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+// @ts-ignore
+
+describe('My First Test', () => {
+  it('Visits Localhost', () => {
+    cy.visit('localhost:3000');
+
+    cy.contains('h1', 'Welcome to Next.js!');
   })
 })
